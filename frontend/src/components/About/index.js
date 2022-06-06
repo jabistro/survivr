820188cc18e88546ea214fec549d9bc7b5880355
John Allan Hinds <jabistro@gmail.com>
2022-06-06 00:21:45 -0400

    final pre-seed push?

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -8,6 +8,14 @@ function About() {
             <p className='about-blurb'>
                 "survivr" was single-handedly built by App Academy student John Allan Hinds with the help of a dozen other students.
             </p>
+            <div className='about-links'>
+                <a className='about-linkdin-link' href='https://www.linkedin.com/in/john-allan-hinds-2aba11237/'>Linkdin
+                    <i id='links' className="fa-brands fa-xl fa-linkedin"></i>
+                </a>
+                <a className='about-github-link' href='https://github.com/jabistro'>Github
+                    <i id='links' className="fa-brands fa-xl fa-github"></i>
+                </a>
+            </div>
         </div>
     );
 }
