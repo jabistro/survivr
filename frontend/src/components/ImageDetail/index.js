3f4ccb9eb3e3bf1d623ba809a19e29257b7419bf
John Allan Hinds <jabistro@gmail.com>
2022-08-19 23:13:39 -0400

    image detail refactor start

--- frontend/src/components/ImageDetail/index.js
+++ frontend/src/components/ImageDetail/index.js
@@ -1,5 +1,6 @@
-import { useSelector } from 'react-redux'
-import { useHistory, useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+import { BiArrowBack } from 'react-icons/bi';
 // import CommentDetail from '../CommentDetail'
 import './ImageDetail.css'
 
@@ -36,18 +37,33 @@ const ImageDetail = () => {
     return (
         <>
             {image &&
-                <div id='img-detail-container'>
-                    <img id='img-detail-img' src={image.imageURL}></img>
-                    <div className='img-detail-info'>
-                        {/* {sessionUser && sessionUser.id !== image.userId && <h2 className='poster'>{image.imageUsername}</h2>} */}
-                        <h2 className='img-detail-caption'>{image.caption}</h2>
-                        <h3 className='img-detail-date'>{month[date.getMonth()]}{date.getDay() - 1}, {date.getFullYear()}</h3>
+                <div className='img-detail-wrap'>
+                    <div className='img-detail-back'>
+                        <div className='img-detail-back-btn-and-words' onClick={() => history.goBack()}>
+                            <BiArrowBack className='img-detail-back-btn' />
+                            <p className='img-detail-back-words'>Back to photos</p>
+                        </div>
                     </div>
-                    {sessionUser && sessionUser.id === image.userId && <button className='photo-detail-edit-button' onClick={() => editHandler(image)}>EDIT</button>}
-                    {/* <CommentDetail /> */}
+                    <img className='img-detail-img' alt='' src={image.imageURL} />
+                    <div className='img-detail-info'>Info</div>
                 </div>
             }
         </>
+
+        // <>
+        //     {image &&
+        //         <div id='img-detail-container'>
+        //             <img id='img-detail-img' src={image.imageURL}></img>
+        //             <div className='img-detail-info'>
+        //                 {/* {sessionUser && sessionUser.id !== image.userId && <h2 className='poster'>{image.imageUsername}</h2>} */}
+        //                 <h2 className='img-detail-caption'>{image.caption}</h2>
+        //                 <h3 className='img-detail-date'>{month[date.getMonth()]}{date.getDay() - 1}, {date.getFullYear()}</h3>
+        //             </div>
+        //             {sessionUser && sessionUser.id === image.userId && <button className='photo-detail-edit-button' onClick={() => editHandler(image)}>EDIT</button>}
+        //             {/* <CommentDetail /> */}
+        //         </div>
+        //     }
+        // </>
     )
 }
 
